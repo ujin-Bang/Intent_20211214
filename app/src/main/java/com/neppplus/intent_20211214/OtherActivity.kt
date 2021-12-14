24ffd77b641d286e82ea8e8f07596e9de6bafa05
ujin-Bang <qkddnwls14@gmail.com>
2021-12-14 19:13:18 +0900

    finish() 화면 종료 처리로 복귀 구현

--- app/src/main/java/com/neppplus/intent_20211214/OtherActivity.kt
+++ app/src/main/java/com/neppplus/intent_20211214/OtherActivity.kt
@@ -13,8 +13,12 @@ class OtherActivity : AppCompatActivity() {
 //        복귀 버튼 눌리면 -> 메인화면으로 이동
         btnReturnToMain.setOnClickListener {
 
-            val myIntent = Intent(this,MainActivity::class.java)
-            startActivity(myIntent)
+//            새 Main화면을 추가해주는 동작 (복귀가 아님)
+//            val myIntent = Intent(this,MainActivity::class.java)
+//            startActivity(myIntent)
+
+//            이 화면을 종료처리. 밑에 있는 메인화면이 나타남.
+            finish()
 
         }
     }
