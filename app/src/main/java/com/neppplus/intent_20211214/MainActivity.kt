f47d50f75d858128b60d041d1c742d13a4908fd7
ujin-Bang <qkddnwls14@gmail.com>
2021-12-15 10:43:24 +0900

    메인 화면 복귀 - 새 닉네임 반영  - onActivityResult 함수  - request종류 구별 if  - result종류 구별 if  - data에서 StringExtra꺼내서 사용

--- app/src/main/java/com/neppplus/intent_20211214/MainActivity.kt
+++ app/src/main/java/com/neppplus/intent_20211214/MainActivity.kt
@@ -3,6 +3,7 @@ package com.neppplus.intent_20211214
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -47,6 +48,32 @@ class MainActivity : AppCompatActivity() {
 //            넘어갔다가 (어떤 데이터)를 데이터를 들고 "돌아올 것이다" 명시.
             startActivityForResult( myIntent, REQ_CODE_FOR_NICKNAME)
 
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+//        결과를 받아서 돌아오면 실행되는 함수
+
+        Log.d("메인화면","결과를 받아오면 무조건 실행됨")
+
+//        구별해야 할 요소 1) 어떤 걸 가지러 다녀온 건지? -requestCode 2) 확인/ 취소 구별 3) 첨부한 데이터
+
+        Log.d("리퀘스트코드",requestCode.toString())
+
+//        닉네임을 가지러 다녀온게 맞는가?
+        if(requestCode == REQ_CODE_FOR_NICKNAME) {
+
+//            확인이 눌린게 맞는가?
+            if( resultCode == RESULT_OK ) {
+
+//                담아준 결과용 Intent => data에 담겨 있따. =>"nick"으로 이름 붙인 String을 꺼내주자.
+                val newNickname = data?.getStringExtra("nick")
+
+                txtNickname.text = newNickname
+
+            }
         }
     }
 }
