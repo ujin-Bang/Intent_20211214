a3c95c605c8d339c525ec94a4c5e04657d9193ed
ujin-Bang <qkddnwls14@gmail.com>
2021-12-15 10:58:51 +0900

    코드 줄바꿈 정리만 함

--- app/src/main/java/com/neppplus/intent_20211214/MainActivity.kt
+++ app/src/main/java/com/neppplus/intent_20211214/MainActivity.kt
@@ -18,7 +18,7 @@ class MainActivity : AppCompatActivity() {
 //            버튼이 눌리면 > OtherActivity로 이동하자. (화면 전환)
 
 //            Intent로 어디로 출발 / 어디로 도착하는지 정보 설정 => 변수에 저장.
-            val myIntent = Intent( this, OtherActivity::class.java )
+            val myIntent = Intent(this, OtherActivity::class.java)
 //            출발 / 도착 정보를 가지고 이동
             startActivity(myIntent)
 
@@ -32,9 +32,9 @@ class MainActivity : AppCompatActivity() {
 
 //            메세지 화면으로 이동. Intent 활용
             val myIntent = Intent(this, ViewMessageActivity::class.java)
-           
+
 //            필요 데이터를 첨부하는 코드
-            myIntent.putExtra("message",inputMessage) //name은 아무렇게나 써도 상관없음
+            myIntent.putExtra("message", inputMessage) //name은 아무렇게나 써도 상관없음
             startActivity(myIntent)
 
         }
@@ -43,10 +43,10 @@ class MainActivity : AppCompatActivity() {
 
 //            닉네임을 변경하기 위한 화면 이동. (Intent 3 예시)
 
-            val myIntent = Intent(this,EditNicknameActivity::class.java)
+            val myIntent = Intent(this, EditNicknameActivity::class.java)
 
 //            넘어갔다가 (어떤 데이터)를 데이터를 들고 "돌아올 것이다" 명시.
-            startActivityForResult( myIntent, REQ_CODE_FOR_NICKNAME)
+            startActivityForResult(myIntent, REQ_CODE_FOR_NICKNAME)
 
         }
     }
@@ -56,17 +56,17 @@ class MainActivity : AppCompatActivity() {
 
 //        결과를 받아서 돌아오면 실행되는 함수
 
-        Log.d("메인화면","결과를 받아오면 무조건 실행됨")
+        Log.d("메인화면", "결과를 받아오면 무조건 실행됨")
 
 //        구별해야 할 요소 1) 어떤 걸 가지러 다녀온 건지? -requestCode 2) 확인/ 취소 구별 3) 첨부한 데이터
 
-        Log.d("리퀘스트코드",requestCode.toString())
+        Log.d("리퀘스트코드", requestCode.toString())
 
 //        닉네임을 가지러 다녀온게 맞는가?
-        if(requestCode == REQ_CODE_FOR_NICKNAME) {
+        if (requestCode == REQ_CODE_FOR_NICKNAME) {
 
 //            확인이 눌린게 맞는가?
-            if( resultCode == RESULT_OK ) {
+            if (resultCode == RESULT_OK) {
 
 //                담아준 결과용 Intent => data에 담겨 있따. =>"nick"으로 이름 붙인 String을 꺼내주자.
                 val newNickname = data?.getStringExtra("nick")
