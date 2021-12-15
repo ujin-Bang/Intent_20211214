ca08bd6e1679a84bdd871da26bffd44635de180f
ujin-Bang <qkddnwls14@gmail.com>
2021-12-15 11:52:34 +0900

    카카오톡 플레이 스토어 다운로드 링크

--- app/src/main/java/com/neppplus/intent_20211214/MainActivity.kt
+++ app/src/main/java/com/neppplus/intent_20211214/MainActivity.kt
@@ -96,6 +96,13 @@ class MainActivity : AppCompatActivity() {
 
 
         }
+
+        btnKakaoStore.setOnClickListener {
+            val myUri = Uri.parse("market://details?id=com.kakao.talk")
+            val myIntent = Intent( Intent.ACTION_VIEW, myUri )
+            startActivity(myIntent)
+
+        }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
